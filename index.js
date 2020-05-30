d74ad65d1c2a22730e4b9c62b1caad36ea856e62
rohit231095 <47350759+rohit231095@users.noreply.github.com>
2020-05-30 17:20:37 +0530

    Removed button

--- index.js
+++ index.js
@@ -5,20 +5,16 @@ const { AppleAuthentication } = NativeModules;
 
 export const RNSignInWithAppleButton = requireNativeComponent('RNCSignInWithAppleButton');
 
-export const SignInWithAppleButton = (buttonStyle, callBack) => {
+export const SignInWithAppleButton = async (buttonStyle, callBack) => {
   if(Platform.OS === 'ios'){
-    return <RNSignInWithAppleButton style={buttonStyle} onPress={async () => {
-    
-        await AppleAuthentication.requestAsync({
-          requestedScopes: [AppleAuthentication.Scope.FULL_NAME, AppleAuthentication.Scope.EMAIL],
+    return await AppleAuthentication.requestAsync({
+          requestedScopes: [],
         }).then((response) => {
           callBack(response) //Display response
           }, (error) => {
             callBack(error) //Display error
            
         });
-
-  }} />
   }else{
   return null
 
